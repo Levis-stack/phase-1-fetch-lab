1ede29509570457717b989745d8746bf8a569934
Levis <Levis.mwangi@student.moringaschool.com>
2025-03-20 11:13:49 +0300

    add changes to index.js file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json));
+  return fetch('https://anapioficeandfire.com/api/books')
 }
 
 function renderBooks(books) {
@@ -15,3 +17,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
